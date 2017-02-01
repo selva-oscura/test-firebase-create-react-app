4a6d31024cd65ac425720d6139b4d01031479180
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-01 12:16:33 -0800

    created firebase configs as export; NOTE: fake data (tracking of this file will cease after this commit);

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "SUPER-SECRET",
+  authDomain: "NOT-THAT-SECRET.firebaseapp.com",
+  databaseURL: "https://not-that-secret.firebaseio.com",
+  storageBucket: "not-that-secret.appspot.com",
+  messagingSenderId: "SUPER-SECRET"
+};
+
+export default firebaseConfig;
+
