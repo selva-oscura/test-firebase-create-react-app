ba8d3c50b1284beaaba0fe4f7220ed4c63a5ae8a
Carol St.Louis <1.selva.oscura@gmail.com>
2017-02-01 12:19:00 -0800

    imported firebase and firebaseConfigs; initialized firebase;

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import firebaseConfig from './firebaseConfig.js';
+import * as firebase from 'firebase';
 
+// Initialize Firebase
+const fb = firebase  
+  .initializeApp(firebaseConfig)
+  .database()
+  .ref();
+
+const App = (props) => {  
+  console.log('snapshot', props);
+  return (
+  	<div className="myApp">
+	    <h1>My Prototype</h1>
+	    <p>{JSON.stringify(props)}</p>
+	   </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
